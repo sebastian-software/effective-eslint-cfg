b95cde40c46b346f23a5a9df249c19e2f2b49811
Sebastian Werner <s.werner@sebastian-software.de>
2024-12-18 17:07:21 +0100

    fix: renamed files

--- src/generator.test.ts
+++ src/generator.test.ts
@@ -1,11 +1,11 @@
 import { describe, expect, it } from "vitest";
-import { createConfig } from "./index";
+import { buildConfig } from "./generator";
 
 const cwd = process.cwd();
 
 describe("createConfig", () => {
   it("generates a base configuration", async () => {
-    const config = await createConfig({ root: cwd });
+    const config = await buildConfig({});
     expect(config).toMatchSnapshot();
   });
 
@@ -14,12 +14,12 @@ describe("createConfig", () => {
   //
 
   it("includes Node-specific rules", async () => {
-    const config = await createConfig({ root: cwd, node: true });
+    const config = await buildConfig({ node: true });
     expect(config).toMatchSnapshot();
   });
 
   it("includes React-specific rules", async () => {
-    const config = await createConfig({ root: cwd, react: true });
+    const config = await buildConfig({ react: true });
     expect(config).toMatchSnapshot();
   });
 
@@ -28,17 +28,17 @@ describe("createConfig", () => {
   //
 
   it("enables strict rules", async () => {
-    const config = await createConfig({ root: cwd, strict: true });
+    const config = await buildConfig({ strict: true });
     expect(config).toMatchSnapshot();
   });
 
   it("applies stylistic rules", async () => {
-    const config = await createConfig({ root: cwd, style: true });
+    const config = await buildConfig({ style: true });
     expect(config).toMatchSnapshot();
   });
 
   it("disable type-based rules", async () => {
-    const config = await createConfig({ root: cwd, fast: true });
+    const config = await buildConfig({ fast: true });
     expect(config).toMatchSnapshot();
   });
 
@@ -47,13 +47,12 @@ describe("createConfig", () => {
   //
 
   it("excludes rules covered by Biome", async () => {
-    const config = await createConfig({ root: cwd, biome: true });
+    const config = await buildConfig({ biome: true });
     expect(config).toMatchSnapshot();
   });
 
   it("combines all options", async () => {
-    const config = await createConfig({
-      root: cwd,
+    const config = await buildConfig({
       node: true,
       react: true,
       strict: true,
---
+++ src/generator.ts
@@ -0,0 +1,169 @@
+import eslint from "@eslint/js";
+import tseslint, { ConfigWithExtends } from "typescript-eslint";
+import { ESLint, Linter } from "eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintReact from "eslint-plugin-react";
+import eslintReactHooks from "eslint-plugin-react-hooks";
+import eslintBiome from "eslint-config-biome";
+import eslintReactCompiler from "eslint-plugin-react-compiler";
+import eslintJsdoc from "eslint-plugin-jsdoc";
+import eslintRegexp from "eslint-plugin-regexp";
+import nodePlugin from "eslint-plugin-n";
+
+interface RuleOptions {
+  /** enable NodeJS checks */
+  node?: boolean;
+
+  /* enable React related rules */
+  react?: boolean;
+
+  /* enable strict checks - recommended */
+  strict?: boolean;
+
+  /* opinionated, best practice, preferring simpler code bases */
+  style?: boolean;
+
+  /** disable type-based rules for fast execution */
+  fast?: boolean;
+
+  /* remove all rules which are supported by Biome to make everything faster */
+  biome?: boolean;
+}
+
+export type FlatConfig = ReturnType<typeof tseslint.config>;
+export type ExtendsList = Parameters<typeof tseslint.config>;
+export type ConfigParam = ESLint.Options["overrideConfig"];
+
+const reactFlat = eslintReact.configs.flat;
+
+export async function buildConfig(options: RuleOptions): Promise<FlatConfig> {
+  const { fast, node, react, strict, style, biome } = options;
+
+  const extendsList: ExtendsList = [eslint.configs.recommended];
+
+  extendsList.push(
+    strict
+      ? // Contains all of recommended, recommended-type-checked, and strict,
+        // along with additional strict rules that require type information.
+        tseslint.configs.strictTypeChecked
+      : // Contains all of recommended along with additional recommended rules
+        // that require type information. Rules newly added in this configuration
+        // are similarly useful to those in recommended.
+        tseslint.configs.recommendedTypeChecked
+  );
+
+  if (style) {
+    // Rules considered to be best practice for modern TypeScript codebases,
+    // but that do not impact program logic. These rules are generally opinionated
+    // about enforcing simpler code patterns.
+    extendsList.push(tseslint.configs.stylisticTypeChecked);
+  }
+
+  if (react && reactFlat) {
+    // Note: The cast is required, because of some TS voodoo with the recommended config from React
+    extendsList.push(reactFlat.recommended as ConfigWithExtends);
+
+    extendsList.push({
+      plugins: {
+        "react-hooks": eslintReactHooks,
+        "react-compiler": eslintReactCompiler,
+      },
+      rules: {
+        ...reactFlat["jsx-runtime"].rules,
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "error",
+        "react-compiler/react-compiler": "error",
+      },
+    });
+  }
+
+  // We like JSDoc but for nothing which can be done better with TypeScript
+  extendsList.push(eslintJsdoc.configs["flat/recommended-typescript-error"]);
+
+  // Check some validity related to usage and definition of regular expressions
+  extendsList.push(eslintRegexp.configs["flat/recommended"]);
+
+  // Check NodeJS things (ESM mode)
+  if (node) {
+    extendsList.push(nodePlugin.configs["flat/recommended-module"]);
+  }
+
+  // Disable all type checked rules for faster runtime of the config e.g. for editor usage etc.
+  if (fast) {
+    extendsList.push(tseslint.configs.disableTypeChecked);
+  } else {
+    extendsList.push({
+      languageOptions: {
+        parserOptions: {
+          projectService: true,
+        },
+      },
+    });
+  }
+
+  // Always disable rules which are better enforced by Prettier
+  extendsList.push(eslintConfigPrettier);
+
+  // When users switch to Biome (for performance) it makes sense to
+  // move these rules from the plate of ESLint to focus on running
+  // complex or type-based rules only.
+  if (biome) {
+    extendsList.push(eslintBiome);
+  }
+
+  const config = tseslint.config(extendsList);
+
+  const linter = new ESLint({
+    overrideConfigFile: true,
+    overrideConfig: config as ConfigParam,
+  });
+
+  const generatedConfig = await linter.calculateConfigForFile("test.tsx");
+  cleanupRules(generatedConfig);
+
+  return generatedConfig;
+}
+
+export function ruleSorter(a: string, b: string) {
+  if (a.includes("/") && !b.includes("/")) {
+    return 1;
+  }
+
+  if (!a.includes("/") && b.includes("/")) {
+    return -1;
+  }
+
+  return a.localeCompare(b);
+}
+
+function cleanupRules(generatedConfig: FlatConfig[number]) {
+  const rules = generatedConfig.rules;
+  if (!rules) {
+    return generatedConfig;
+  }
+
+  const ruleNames = Object.keys(rules).sort(ruleSorter);
+
+  const cleanRules: typeof rules = {};
+  for (const ruleName of ruleNames) {
+    const value = rules[ruleName];
+    if (value != null && Array.isArray(value)) {
+      const level = value[0];
+      if (level === 0) {
+        // pass, ignore
+      } else {
+        const levelStr = level === 2 ? "error" : "warn";
+
+        if (value.length === 1) {
+          cleanRules[ruleName] = levelStr;
+        } else {
+          cleanRules[ruleName] = [levelStr, ...value.slice(1)];
+        }
+      }
+    }
+  }
+
+  generatedConfig.rules = cleanRules;
+
+  return generatedConfig;
+}
