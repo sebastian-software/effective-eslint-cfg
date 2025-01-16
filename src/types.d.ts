05b3348552ea0a4d297e0482519aed9a1e879060
Sebastian Werner <s.werner@sebastian-software.de>
2025-01-16 12:43:56 +0100

    fix: issues with type-def

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,5 @@
 declare module "eslint-config-prettier" {
+  import { Linter } from "eslint"
   const config: {
     rules: Linter.Rules
   }
@@ -6,9 +7,18 @@ declare module "eslint-config-prettier" {
 }
 
 declare module "eslint-plugin-react-hooks" {
+  import { Linter } from "eslint"
   export const rules: Linter.Rules
 }
 
 declare module "eslint-plugin-react-compiler" {
+  import { Linter } from "eslint"
   export const rules: Linter.Rules
+}
+
+declare module "eslint-plugin-jsx-a11y" {
+  import type { ConfigWithExtends } from "typescript-eslint"
+
+  export const rules: Linter.Rules
+  export const flatConfigs: Record<string, ConfigWithExtends>
 }
